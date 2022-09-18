8ce09563ddee195c953192f12cbab4307978f938
przemyslaw.zygmanowski <przemyslaw.zygmanowski@makolab.com>
2022-09-18 21:52:02 +0200

    clean project

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  const MONGO_URI = `mongodb+srv://przemek:kozQ30SNiwTddgMv@cluster0.vjsrrbz.mongodb.net/bh?retryWrites=true&w=majority`;
-  const conn = await mongoose.connect(MONGO_URI, {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
   });
   console.log(`mongo connected ${conn.connection.host}`.cyan);
